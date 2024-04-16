3857f53ad28ffdb6ea72a65acddfbc142d5a5aef
maria clara bassedon <118577937+bassedon041@users.noreply.github.com>
2024-04-16 19:36:57 -0300

    çç

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-const botoes = document.querySelectorAll("botao");
-console.log(".botoes")
+const botoes = document.querySelectorAll(".botao");
+
+console.log(botoes)
 
 for (let i=0;i <botoes.length;i++) {
     botoes[i].onclick = function(){
